bf0479e45109ce0fbed153df75650ebf9f68c04c
Tsundzukani <trnghondzweni@gmail.com>
2019-05-20 09:04:42 +0200

    Mongo switch

--- app.js
+++ app.js
@@ -10,162 +10,100 @@ var bodyParser = require('body-parser');
 var expressValidator = require('express-validator');
 var expressSession = require('express-session');
 var MySQLStore = require('express-mysql-session')(expressSession);
-var setup = require('./controllers/admin')
+var mongoose = require('mongoose');
+var setup = require('./controllers/admin');
+const config = require('./config/database');
+const session = require('express-session');
+const MongoSotre = require('connect-mongo')(session)
 // var transporter = require('nodemailer'); 
-// var nocache = require('nocache')
 
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
-// var adminRouter = require('./routes/admin');
-
-
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
-// var adminRouter = require('./routes/admin');
 
 var app = express();
 
 
-
-
-var usersController = require('./controllers/users');
-var adminController = require('./controllers/admin');
-var userSettingsController = require('./controllers/userSettings');
-
-
-//Setup database connection
-module.exports.adminDB = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '123456',
+mongoose.connect(config.database, { useCreateIndex: true, useNewUrlParser: true});
+var db = mongoose.connection;
+db.once('open', function() {
+	console.log('Connected to MongoDB');
+});
+db.on('error', function(err) {
+	console.log(err);
 });
 
-
-//create sql connection
-module.exports.db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '123456',
-  database: 'matcha'
-});
-
-// setup.setup();
-var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '123456',
-  database: 'matcha'
-});
-
-module.exports.sessionStore = new MySQLStore({
-  clearExpired: true,
-  checkExpirationInterval: 900000,
-  expiration: 86400000,
-  createDatabaseTable: true,
-  connectionLimit: 1,
-  endConnectionOnClose: true,
-  charset: 'utf8mb4_bin',
-  schema: {
-      tableName: 'sessions',
-      columnNames: {
-          session_id: 'session_id',
-          expires: 'expires',
-          data: 'data'
-      }
-  }}/* session store options */, connection);
+app.use(session({
+	secret: "abcd",
+	resave: false,
+	saveUninitialized: false,
+	store: new MongoSotre({ mongooseConnection: mongoose.connection}),
+	// cookie: {maxAge: 180 * 60 * 1000}
+}));
 
 
-// connect
-// db.connect(function (err) {
-  //   if (err) {
-    //     console.error('error connecting: ' + err.stack);
-    //     return;
-    //   }
-    //   console.log('connected as id ' + db.threadId);
-    // });
-    
-    
-    app.use(expressSession({secret: 'max', store: module.exports.sessionStore = new MySQLStore({
-      clearExpired: true,
-      checkExpirationInterval: 900000,
-      expiration: 86400000,
-      createDatabaseTable: true,
-      connectionLimit: 1,
-      endConnectionOnClose: true,
-      charset: 'utf8mb4_bin',
-      schema: {
-        tableName: 'sessions',
-        columnNames: {
-          session_id: 'session_id',
-          expires: 'expires',
-          data: 'data'
-        }
-      }}/* session store options */, connection)
-      , saveUninitialized: false, resave: false}));
 
-      app.use(expressValidator());
-      app.use(bodyParser.urlencoded({extended : true}));
-      app.use(logger('dev'));
-      app.use(express.json());
-      app.use(express.urlencoded({
-        extended: false
-        
-      }));
-      app.use(cookieParser());
-      app.use(express.static(path.join(__dirname, 'public')));
-      
-      
-      /* ===============================
-      -   GETS
-      ================================*/
-      
-      app.get("/register", usersController.register);
-      app.get("/login", usersController.login);
-      app.get("/setup", adminController.setup);
-      app.get("/settings",userSettingsController.modifyInfo);
-      app.get("/home", usersController.home);
-      app.get("/", usersController.home);
-      app.get("/logout", usersController.logout);
-      app.get("/verification/:type/:key", usersController.verificationFunction)
-      
-      
-      /* ===============================
-      -   POSTS
-      ================================*/
-      
-      app.post("/registerValidation", usersController.registerValidation);
-      app.post("/loginValidation", usersController.loginValidation);
-      app.post("/settings/executeModifyInfo", userSettingsController.executeModifyInfo);
+  app.use(expressValidator());
+  app.use(bodyParser.urlencoded({extended : true}));
+  app.use(logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false  }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
+  
+
+  app.use(bodyParser.json());
+
+
+  // view engine setup
+  app.engine('hbs', hbs({
+    extname: 'hbs',
+    defaultLayout: 'layout',
+    layoutsDir: __dirname + '/views/layouts'
+  }));
+  app.set('views', path.join(__dirname, 'views/layouts'));
+  app.set('view engine', 'hbs');
+
+
+  var usersController = require('./controllers/users');
+  var adminController = require('./controllers/admin');
+  var userSettingsController = require('./controllers/userSettings');
   
   
-      // view engine setup
-      app.engine('hbs', hbs({
-        extname: 'hbs',
-        defaultLayout: 'layout',
-        layoutsDir: __dirname + '/views/layouts'
-      }));
-      app.set('views', path.join(__dirname, 'views/layouts'));
-      app.set('view engine', 'hbs');
-      
-      
-      // app.use('/', indexRouter); //mount index route at / path
-      // app.use('/users', usersRouter);
-      // app.use('/admin', adminRouter);
-      
-      // catch 404 and forward to error handler
-      app.use(function (req, res, next) {
-        next(createError(404));
-      });
-      
-      // error handler
-      app.use(function (err, req, res, next) {
-        // set locals, only providing error in development
-        res.locals.message = err.message;
-        res.locals.error = req.app.get('env') === 'development' ? err : {};
-        
-        // render the error page
-        res.status(err.status || 500);
-        res.render('error');
-      });
-      
-      module.exports = app;
+  /* ===============================
+  -   GETS
+  ================================*/
+  
+  app.get("/register", usersController.register);
+  app.get("/login", usersController.login);
+  app.get("/setup", adminController.setup);
+  app.get("/settings",userSettingsController.modifyInfo);
+  app.get("/home", usersController.home);
+  app.get("/", usersController.home);
+  app.get("/logout", usersController.logout);
+  app.get("/verification/:type/:key", usersController.verificationFunction)
+  
+  
+  /* ===============================
+  -   POSTS
+  ================================*/
+  
+  app.post("/registerValidation", usersController.registerValidation);
+  app.post("/loginValidation", usersController.loginValidation);
+  app.post("/settings/executeModifyInfo", userSettingsController.executeModifyInfo);
+
+  
+  // catch 404 and forward to error handler
+  app.use(function (req, res, next) {
+    next(createError(404));
+  });
+  
+  // error handler
+  app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+  
+  module.exports = app;
